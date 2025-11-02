53c3b07de3499e55421cad60bc83e353b546f020
MadHatterCode <sadante@gmail.com>
2025-11-02 19:06:43 +0200

    HW_1

---
+++ index.js
@@ -0,0 +1,32 @@
+// #dYQNrBV
+
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let nnn = -999;
+let oneTwoThree = 123;
+const PI = 3.14
+let twoSeven = 2.7;
+let sixteen = 16;
+let positive = true;
+let negative = false
+
+console.log(hello, owu, com, ua, one, ten, nnn, oneTwoThree, PI, twoSeven, sixteen, positive, negative);
+
+// #6Qb97gsv
+let firstName = 'Sergii';
+let middleName = 'Oleksandrovich';
+let lastName = 'Bezsalyi';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+// #4N0y5tufA
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, typeof b, typeof c);
