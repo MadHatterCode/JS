98c77e31fa7d9afef42a47ae03fcba4e88db4a29
MadHatterCode <sadante@gmail.com>
2025-12-12 16:38:48 +0200

    continued HW_10

--- HW_10.js
+++ HW_10.js
@@ -21,32 +21,68 @@
 // }
 
 
-// let sessionList = JSON.parse(localStorage.getItem('sessions')) || [];
-// let pageName = window.location.pathname;
-// switch (pageName) {
-//     case '/JS/index.html':
-//         let date = new Date();
-//         sessionList.push(date)
-//         localStorage.setItem('sessions', JSON.stringify(sessionList));
-//         break;
-//     case '/JS/sessionsListPrint.html':
-//         const list = document.createElement("ul")
-//         sessionList.forEach(e => {
-//             let li = document.createElement('li');
-//             li.innerText = e;
-//             list.appendChild(li);
-//         })
-//         document.body.appendChild(list);
-//         break
-// }
+let sessionList = JSON.parse(localStorage.getItem('sessions')) || [];
+let pageName = window.location.pathname;
+switch (pageName) {
+    case '/JS/index.html':
+        let date = new Date();
+        sessionList.push(date)
+        localStorage.setItem('sessions', JSON.stringify(sessionList));
+        break;
+    case '/JS/sessionsListPrint.html':
+        const list = document.createElement("ul")
+        sessionList.forEach(e => {
+            let li = document.createElement('li');
+            li.innerText = e;
+            list.appendChild(li);
+        })
+        document.body.appendChild(list);
+        break
+}
 
 
-const converterInput = document.querySelector("#converter");
-const convertedValue = document.querySelector("#converted-value");
+// const converterInput = document.querySelector("#converter");
+// const convertedValue = document.querySelector("#converted-value");
+//
+// converterInput.addEventListener("input", (e) => {
+//     const kg = Number(e.target.value);
+//     convertedValue.textContent = kg ? Math.round(kg * 2.2046) : "";
+// });
 
-converterInput.addEventListener("input", (e) => {
-    const kg = Number(e.target.value);
-    convertedValue.textContent = kg ? Math.round(kg * 2.2046) : "";
-});
 
+const addToLocalStorage = (localArrKey, obj) => {
+    const arrGet = JSON.parse(localStorage.getItem(localArrKey) || '[]');
+    arrGet.push(obj);
+    localStorage.setItem(localArrKey, JSON.stringify(arrGet));
+}
 
+addToLocalStorage('sessions', {name: 'John', surname: 'Localstorage'});
+
+const tableContainer = document.querySelector(".table-creator");
+const tableCreatorForm = document.querySelector(".table-creator form");
+
+const tableCreator = () => {
+    const formFields = ['rows', 'columns', 'cellContent']
+    const formValues = formFields.reduce((acc, fieldName) => {
+        const fieldValue = tableCreatorForm[fieldName].value.trim();
+        acc[fieldName] = fieldName === 'cellContent'? fieldValue : Number(fieldValue);
+        return acc;
+    }, {})
+    const table = document.createElement("table");
+    for(let i = 0; i < formValues.rows; i++) {
+        const row = document.createElement("tr");
+        for(let j = 0; j < formValues.columns; j++) {
+            const cell = document.createElement("td");
+            cell.textContent = formValues.cellContent;
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    tableContainer.appendChild(table);
+
+}
+
+tableCreatorForm.onsubmit = (e) => {
+    e.preventDefault();
+    tableCreator()
+};
